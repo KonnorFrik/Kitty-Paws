e41f08fa5e0b768adc25f599aabcc84cb010dae8
Konnor <konnor.frik666@gmail.com>
2024-07-17 16:23:37 +0500

    Declare some start objects

---
+++ src/paws_data.h
@@ -0,0 +1,29 @@
+/** @file
+ * @brief Data (types, struct, etc) for the app is declared here */
+
+#ifndef __KITTY_PAWS_DATA_H__
+#define __KITTY_PAWS_DATA_H__
+
+#include <stddef.h>
+#include "cvector/cvector.h"
+
+/**
+ * @brief Hold indeces for vertex, texture, normal 
+ */
+typedef struct _paws_face {
+    size_t vertex_index, texture_index, normal_index;
+} paws_face;
+
+/**
+ * @brief Hold mesh loaded from file
+ */
+typedef struct _paws_mesh {
+    cvector* vertices; ///< Contains pointers to Vector3 
+    cvector* textures; ///< Contains pointers to Vector2
+    cvector* normals;  ///< Contains pointers to Vector3
+    cvector* faces;    ///< Contains pointers to paws_face
+} paws_mesh;
+
+bool parse_format_obj(const char* filepath, paws_mesh* mesh);
+
+#endif /* __KITTY_PAWS_DATA_H__ */
