d8d8fe2064fc1c8c33603deb3538d601e265f97a
Konnor <konnor.frik666@gmail.com>
2024-07-19 14:39:04 +0500

    Move creators functions into 'model' folder

---
+++ src/model/paws_mesh_creators.c
@@ -0,0 +1,61 @@
+#include "../paws_data.h"
+
+#include <stdlib.h>
+
+/**
+ * @brief Allocate memory for each member of paws_mesh object
+ * @param[out] mesh a paws_mesh object
+ * @return status: false:OK true:ERROR
+ * @version 0.1.0
+ */
+bool paws_mesh_ctor(paws_mesh* mesh) {
+    bool status = false;
+
+    mesh->vertices = cvector_new(1);
+    mesh->textures = cvector_new(1);
+    mesh->normals = cvector_new(1);
+    mesh->faces = cvector_new(1);
+    mesh->settings.point_type = SPHERE;
+    mesh->settings.color_point = BLACK;
+    mesh->settings.color_line = BLACK;
+
+    if ( !mesh->vertices || 
+         !mesh->textures || 
+         !mesh->normals ||
+         !mesh->faces
+    ) {
+        status = true;
+    }
+
+    return status;
+}
+
+/**
+ * @brief Free allocated memory for each member of paws_mesh object
+ * @param[out] mesh a paws_mesh object
+ * @version 0.1.0
+ */
+void paws_mesh_dtor(paws_mesh* mesh) {
+    for (size_t i = 0; i < cvector_size(mesh->faces); ++i) {
+        cvector* face_indeces = cvector_at(mesh->faces, i);
+
+        if ( !face_indeces ) {
+            continue;
+        }
+
+        for (size_t j = 0; j < cvector_size(face_indeces); ++j) {
+            paws_face_indices* obj = cvector_at(face_indeces, j);
+
+            if ( obj ) {
+                free(obj);
+            }
+        }
+
+        cvector_delete(face_indeces);
+    }
+
+    cvector_delete(mesh->vertices);
+    cvector_delete(mesh->textures);
+    cvector_delete(mesh->normals);
+    cvector_delete(mesh->faces);
+}
