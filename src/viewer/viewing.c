fc6e371bb0d554f2ef798eda2e4a00010a1eb55f
Konnor <konnor.frik666@gmail.com>
2024-07-22 16:48:26 +0500

    Useless changes

--- src/viewer/viewing.c
+++ src/viewer/viewing.c
@@ -4,6 +4,11 @@
 #include <raymath.h>
 
 #define DRAW_DEBUG 0
+#define PRINT_DEBUG 0
+
+#if PRINT_DEBUG == 1
+#include <stdio.h>
+#endif
 
 /** 
  * @brief Draw each face of a mesh (points, edges)
@@ -20,11 +25,16 @@ void draw_mesh(paws_mesh* mesh) {
     Color color_normal = mesh->settings.color_normal;
 
     bool is_draw_normals = mesh->is_draw_normals;
+    bool is_draw_edges = mesh->is_draw_edges;
 
     for (size_t fi = 0; fi < faces_count; ++fi) {
         cvector* faces = cvector_at(mesh->faces, fi);
 
-        size_t indices_count = cvector_size(faces) - 1;
+        size_t indices_count = cvector_size(faces);
+
+        if ( indices_count == 0 ) {
+            continue;
+        }
 
         Vector3* start_vertex = cvector_at(mesh->vertices, ((paws_face_indices*)cvector_at(faces, 0))->vertex_index);
         Vector3* end_vertex = NULL;
@@ -32,22 +42,12 @@ void draw_mesh(paws_mesh* mesh) {
         Vector3 face_mid_point = *start_vertex;
         Vector3 face_normal = *(Vector3*)cvector_at(mesh->normals, ((paws_face_indices*)cvector_at(faces, 0))->normal_index);
 
-        for (size_t vi = 0; vi < indices_count; ++vi) {
+        for (size_t vi = 0; vi < (indices_count - 1); ++vi) {
             paws_face_indices* start_face = cvector_at(faces, vi);
             paws_face_indices* end_face = cvector_at(faces, vi + 1);
 
             Vector3* start_vert = (Vector3*)cvector_at(mesh->vertices, start_face->vertex_index);
             end_vertex = (Vector3*)cvector_at(mesh->vertices, end_face->vertex_index);
-
-            Vector3 normal = *(Vector3*)cvector_at(mesh->normals, end_face->normal_index);
-
-            face_normal.x += normal.x;
-            face_normal.y += normal.y;
-            face_normal.z += normal.z;
-
-            face_mid_point.x += end_vertex->x;
-            face_mid_point.y += end_vertex->y;
-            face_mid_point.z += end_vertex->z;
 
             switch ( point_type ) {
                 default:
@@ -61,19 +61,28 @@ void draw_mesh(paws_mesh* mesh) {
                     break;
             }
 
-            if ( mesh->is_draw_edges ) {
+            if (is_draw_edges ) {
                 DrawLine3D(*start_vert, *end_vertex, color_line);
             }
+
+            face_mid_point.x += end_vertex->x;
+            face_mid_point.y += end_vertex->y;
+            face_mid_point.z += end_vertex->z;
+
+            Vector3 normal = *(Vector3*)cvector_at(mesh->normals, end_face->normal_index);
+            face_normal.x += normal.x;
+            face_normal.y += normal.y;
+            face_normal.z += normal.z;
         }
 
-        if ( mesh->is_draw_edges ) {
+        if ( is_draw_edges ) {
             DrawLine3D(*start_vertex, *end_vertex, color_line);
         }
 
         if ( is_draw_normals ) {
-            face_mid_point.x /= indices_count + 1;
-            face_mid_point.y /= indices_count + 1;
-            face_mid_point.z /= indices_count + 1;
+            face_mid_point.x /= indices_count;
+            face_mid_point.y /= indices_count;
+            face_mid_point.z /= indices_count;
 
             face_normal = Vector3Normalize(face_normal);
             face_normal = Vector3Add(face_mid_point, face_normal);
@@ -81,7 +90,7 @@ void draw_mesh(paws_mesh* mesh) {
             DrawLine3D(face_mid_point, face_normal, color_normal);
 
             #if DRAW_DEBUG == 1
-            DrawSphere(face_mid_point, 0.07, RED);
+            DrawSphere(face_mid_point, 0.04, RED);
             #endif
         }
 
